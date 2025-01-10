3d5c6b8f9f8f25ca16b2c9dd825d0445bf19848f
AustinG <austin.geisler@vtsd.com>
2025-01-10 13:09:49 -0500

    Login button now does things (after giving me a minor headache)

--- 2-1-4/main.py
+++ 2-1-4/main.py
@@ -7,12 +7,18 @@ root.wm_geometry("300x200")
 
 # create empty frame
 frame_login = tk.Frame(root)
+frame_auth = tk.Frame(root)
 
 # create empty frame (new window for components in roots)
-frame_login.grid()
+frame_login.grid(row=0, column=0, sticky="news")
+frame_auth.grid(row=0, column=0, sticky="news")
 
 # Defining font
 standard_font = tkFont.Font(family="Helvetica", size=13, weight="bold")
+
+# Defining functions
+def login():
+    frame_auth.tkraise()
 
 # widgets for frame_login
 # Username lbl
@@ -30,7 +36,13 @@ ent_password = tk.Entry(frame_login, bd=3, show='*')
 ent_password.pack(padx=50)
 
 # Login button
-button_login = tk.Button(frame_login, text='Login', font=standard_font)
+button_login = tk.Button(frame_login, text='Login', command=login, font=standard_font)
 button_login.pack(padx=50, pady=10)
 
+# widgets for frame_auth
+lbl_auth = tk.Label(frame_auth, text='Authorized', font=standard_font)
+lbl_auth.pack()
+
+
+frame_login.tkraise()
 root.mainloop()
